ee245196bf26393f44fba428eb08faceb06b52d0
Jakub Antczak <50321236+jakubsgit@users.noreply.github.com>
2020-12-16 12:40:05 +0100

    Initial commit

---
+++ design-system/theme.tsx
@@ -0,0 +1,11 @@
+import { colors } from "../design-system.framerfx/code/canvas"
+
+const space: string[] = ["0", "4px", "8px", "16px", "32px"]
+
+const font = "-apple-system, BlinkMacSystemFont, Helvetica, Arial, sans-serif"
+
+export const theme = {
+  color: colors,
+  font,
+  space
+}
